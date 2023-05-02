0c460095130efa2d0bb3e2a2fe87c3858f30f8f2
olvaesss <100799717+olvaesss@users.noreply.github.com>
2023-05-02 09:31:25 +0300

    0.0.2

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import {join} from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('Server running:http://localhost:3000/')
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Сообщим приложению, где искать наши views.
+  app.setBaseViewsDir(join(__dirname, '../views'));
+
+  // И укажем, какой шаблонизатор использовать
+  app.setViewEngine('pug');
+  await app.listen(3000)
+  console.log('server running at: http//localhost/3000');
 }
 bootstrap();
